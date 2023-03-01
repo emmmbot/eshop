39e40500221eb93c7507ba2356ca9e5283e83f48
emmmbot <79842802+emmmbot@users.noreply.github.com>
2023-03-01 14:49:25 +0800

    Add files via upload
    sql语句

---
+++ eshop.sql
@@ -0,0 +1,53 @@
+create database ESHOP;
+
+use ehop;
+
+create TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `PASSWORD` varchar(20) NOT NULL,
+  `gender` varchar(2) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `introduce` varchar(100) DEFAULT NULL,
+  `activeCode` varchar(50) DEFAULT NULL,
+  `state` int(11) DEFAULT '0',
+  `role` int(11) DEFAULT '0',
+  `registTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `products` (
+  `id` varchar(100) NOT NULL DEFAULT '',
+  `name` varchar(40) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `category` varchar(40) DEFAULT NULL,
+  `pnum` int(11) DEFAULT NULL,
+  `imgurl` varchar(100) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `orders` (
+  `id` varchar(100) NOT NULL,
+  `money` double DEFAULT NULL,
+  `receiverAddress` varchar(255) DEFAULT NULL,
+  `receiverName` varchar(20) DEFAULT NULL,
+  `receiverPhone` varchar(20) DEFAULT NULL,
+  `paystate` int DEFAULT '0',
+  `trackingnumber` varchar(15) DEFAULT NULL,
+  `ordertime` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+CREATE TABLE `orderitem` (
+  `order_id` varchar(100) NOT NULL DEFAULT '',
+  `product_id` varchar(100) NOT NULL DEFAULT '',
+  `buynum` int(11) DEFAULT NULL,
+  PRIMARY KEY (`order_id`,`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
